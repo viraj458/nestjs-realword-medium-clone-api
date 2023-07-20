9cce994eff9b09f5fe71db0be1559ced6aadb6e9
viraj458 <virajmalaka94@gmail.com>
2023-07-20 14:31:57 +0530

    initial prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
 })
 export class AppModule {}
